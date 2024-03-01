4e46a965aa0fdfb57f9dd6db49aa124df3e595d1
Yknas <tudoreldreglea@mail.ru>
2024-03-01 18:55:53 +0200

    implemented template, controllers and bundles

---
+++ WebApplicationTODO/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplicationTODO.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new StyleBundle("~/assets/css").Include(
+                        "~/assets/css/style-starter.css"));
+
+            bundles.Add(new ScriptBundle("~/assets/js").Include(
+                    "~/assets/js/bootstrap.min.js",
+                    "~/assets/js/circles.js",
+                    "~/assets/js/jquery-3.3.1.min.js",
+                    "~/assets/js/owl.carousel.js",
+                    "~/assets/js/theme-change.js"
+                    ));
+
+        }
+    }
+}
---
+++ WebApplicationTODO/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationTODO.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
